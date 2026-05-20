b12be95f47231870aa6a9e83c5fac8f5f1468d95
Bianca Valenciani <137175735+BiaValen@users.noreply.github.com>
2026-05-19 23:05:20 -0300

    Enhance shutdown message with project information
    Added a shutdown message with project details.

--- minix/kernel/main.c
+++ minix/kernel/main.c
@@ -364,6 +364,12 @@ void prepare_shutdown(const int how)
    * argument passes the shutdown status. 
    */
   printf("MINIX will now be shut down ...\n");
+
+  printf("=======================================\n");
+  printf("| ATE A PROXIMA - SO - UNIFESP 1s2026 |\n");
+  printf("| Projeto Pratico - Equipe 1           |\n");
+  printf("=======================================\n");
+	
   set_kernel_timer(&shutdown_timer, get_monotonic() + system_hz,
       minix_shutdown, how);
 }
